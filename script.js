35f59eff05da7ce47be6295ccab451d9832600ab
carolinagalanm01 <galamarca2019@gmail.com>
2025-05-10 18:17:15 -0600

    Update script.js
    Signed-off-by: carolinagalanm01 <galamarca2019@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function mostrarSeccion(id) {
   const secciones = document.querySelectorAll('section');
-  secciones.forEach(sec => sec.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-}
+  secciones.forEach(sec => sec.classList.remove('
+
